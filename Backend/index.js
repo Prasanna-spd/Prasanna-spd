cf203d113e536057a3f3ce8b0593b6ac649b546f
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-01 09:03:12 +0530

    added a route

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,9 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get("/", (req, res) => {
+  res.send("hey sarda");
+});
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
