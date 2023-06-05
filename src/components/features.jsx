51834e6554102c71306c68f011baf50e6f2e9df1
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-05 23:12:41 +0530

    updated fetch api and added Modal

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,16 +1,46 @@
-import { Box, Button, HStack, Image, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Image,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+
 // import React, { useState } from "react";
 import img1 from "../assets/asset1.jpg";
 import Heading from "./heading";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { FeatureContext } from "./contextmang";
+import Modal from "../Modal";
 
 function Features() {
-  //   const [feature, setFeature] = useState("second");
+  const [isOpen, setisOpen] = useState(false);
   const { selectedfeature } = useContext(FeatureContext);
+  const [response, setResponse] = useState(null);
+
+  const handleClick = async (featureName, brandName) => {
+    await fetch("http://localhost:5000/api/mySelectedfeature", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ feature: featureName, brand: brandName }),
+    })
+      .then(async (res) => {
+        const data = await res.json();
+        setResponse(data);
+        setisOpen(true);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div>
       <Heading />
+      <header>{selectedfeature?.name}</header>
       <Box display="flex" justifyContent={"center"} alignItems={"center"}>
         <HStack>
           <Image
@@ -18,7 +48,7 @@ function Features() {
             h={"600"}
             objectFit={"contain"}
             alt={"Exchange"}
-            src={selectedfeature && selectedfeature.image}
+            src={selectedfeature?.image}
             my={5}
             p={16}
             me={20}
@@ -33,6 +63,7 @@ function Features() {
                   transform: "scale(1.1)",
                 },
               }}
+              onClick={() => handleClick("warranty", selectedfeature?.name)}
             >
               Warranty
             </Button>
@@ -45,6 +76,7 @@ function Features() {
                   transform: "scale(1.1)",
                 },
               }}
+              onClick={() => handleClick("durability", selectedfeature?.name)}
             >
               Durability
             </Button>
@@ -57,6 +89,7 @@ function Features() {
                   transform: "scale(1.1)",
                 },
               }}
+              onClick={() => handleClick("material", selectedfeature?.name)}
             >
               Material
             </Button>
@@ -81,11 +114,17 @@ function Features() {
                   transform: "scale(1.1)",
                 },
               }}
+              onClick={() => handleClick("colors", selectedfeature?.name)}
             >
               Colors
             </Button>
           </VStack>
         </HStack>
+        <div>
+          <Modal open={isOpen} onClose={() => setisOpen(false)}>
+            {{ title: selectedfeature.name, data: response }}
+          </Modal>
+        </div>
       </Box>
     </div>
   );
